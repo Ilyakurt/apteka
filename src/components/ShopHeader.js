c539cd3bccd5d0c3dc93794ca202f1fb303ced04
hovik23 <hovik@tuta.io>
2021-07-15 18:57:24 +0300

    updated

---
+++ src/components/ShopHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShopHeader = () => {
+    return (
+        <div className="shop-header">
+            РосГосМосПромНефтьГазАлмаз Магазин
+        </div>
+    );
+}
+
+export default ShopHeader;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("Как интересно!")
+import React, { useState } from 'react';
+import './App.css';
+import ShopHeader from './components/ShopHeader';
+import ShopBody from './components/ShopBody';
+import ReactDOM from 'react-dom'
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  return (
+    <div>
+      <ShopHeader />
+      <ShopBody items={items} setItems={setItems} />
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
